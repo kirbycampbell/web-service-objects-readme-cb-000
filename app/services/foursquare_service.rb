d627554903c1efccec88ecf214d18af3f18ccbc8
Kirby Campbell <jkirbycampbell@gmail.com>
2018-10-23 20:36:16 +0000

    automatically backed up by learn

--- app/services/foursquare_service.rb
+++ app/services/foursquare_service.rb
@@ -15,11 +15,11 @@ class FoursquareService
   end
 
   def friends(token)
-  resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
-    req.params['oauth_token'] = token
-    req.params['v'] = '20160201'
+    resp = Faraday.get("https://api.foursquare.com/v2/users/self/friends") do |req|
+      req.params['oauth_token'] = token
+      req.params['v'] = '20160201'
+    end
+    JSON.parse(resp.body)["response"]["friends"]["items"]
   end
-  JSON.parse(resp.body)["response"]["friends"]["items"]
-end
 
 end
